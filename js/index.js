4354cd938c8746a08088250750e3c4f02f6f32c0
mori-his <zhangss5210@163.com>
2016-01-15 12:52:16 +0800

    updata js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,36 @@
 $(function () {
-    $('.content').css({
-        height: $(window).height()
+    var speed = 10,
+        stop = 13000;
+
+    $('.list-group').each(function (index) {
+        if (index == 0) {
+            $(this).css({
+                top: 0
+            });
+        } else {
+            var top = parseInt($('.list-group:eq(' + (index - 1) + ')').css('top')) + $(this).height();
+            $(this).css({
+                top: top
+            });
+        }
     });
-    $(window).on('resize scroll', function () {
-        $('.content').css({
-            height: $(window).height()
+    var index = 0;
+    var isBegin = false;
+    $('.start').on('click', function () {
+        console.log('index', index);
+        if (isBegin) {
+            return false;
+        }
+        isBegin = true;
+        $('.list-group').eq(index).css({
+            top: parseInt($('.list-group').eq(index).css('top')) - $('.list-group').eq(index).height()
         });
-    })
+        $('.list-group').eq(index + 1).css({
+            top: parseInt($('.list-group').eq(index).css('top')) + $('.list-group').eq(index).height()
+        })
+
+        isBegin = false;
+        index += 1;
+
+    });
 });
