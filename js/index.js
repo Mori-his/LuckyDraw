202ba6596faad7ef20ca58a1ae6ef89b1813bdd7
mori-his <zhangss5210@163.com>
2016-01-26 11:21:28 +0800

    devS1 updata

--- js/index.js
+++ js/index.js
@@ -1,107 +1,85 @@
 $(function () {
 
     initList();
-    var duration = 70,//动画执行的毫秒,注意:不要大于步长(speed)的毫秒数
-        speed = 100,//步长
+    var duration = 50,//动画执行的毫秒,注意:不要大于步长(speed)的毫秒数
+        speed = 80,//步长
         stop = 5000;//停止时间
 
 
-
-    var index = 0;//初始化当前下标
     var isBegin = false;//阻止连续点击
 
-    $('.start').on('click', function () {
+    function lottery(){
         if (isBegin) {
             return false;
         }
-
+        initList();
         isBegin = true;
         var _list = $('.list-group');
-        stop = _list.length * speed * 10;
+
         var numRan = parseInt((Math.random() + .1) * _list.length);
-        //console.log(numRan);
-        stop += numRan * speed;
-        if(index != 0){
-            _list.eq(index).animate({
-                top: -_list.eq(index).height()
-            },{
-                duration: duration,
-                complete: function(){
-                    _list.eq(index).css({
-                        top: _list.eq(index).height()
-                    });
-                    index = 0;
-                }
-            })
-        }
 
-        var timer = setInterval(function () {
-            if (index >= _list.length - 1) {
-
-                index = 0;
-
-                _list.eq(_list.length - 1).animate({
-                    top: parseInt(_list.eq(_list.length - 1).css('top')) - (_list.eq(index).height() * (index + 1))
-                }, {
-                    duration: duration,
-                    complete: function () {
-                        _list.eq(_list.length - 1).css({
-                            top: _list.eq(index).height()
-                        });
-                    }
+        //第一次进来的时候第一个往上移走
+        _list.eq(0).animate({
+            top: -parseInt(_list.height())
+        }, {
+            duration: duration,
+            complete: function () {
+                _list.eq(0).css({
+                    top: _list.eq(0).height()
                 });
-
-                _list.eq(index).animate({
-                    top: 0
-                }, {
-                    duration: duration
-                });
-
-            } else {
-
-                _list.eq(index).animate({
-                    top: parseInt(_list.eq(index).css('top')) - _list.eq(index).height()
-                }, {
-                    duration: duration,
-                    complete: function () {
-                        _list.eq(index).css({
-                            top: _list.eq(index).height()
-                        });
-                    }
-                });
-
-                _list.eq(index + 1).animate({
-                    top: 0
-                },{
-                    duration: duration,
-                    complete: function(){
-                        //isBegin = false;
-                        index += 1;
-                    }
-                });
-
             }
-
+        });
+        var timer = setInterval(function () {
+            var numRanID = parseInt((Math.random() + .1) * _list.length);
+            _list.eq(numRanID).animate({
+                top: parseInt(_list.eq(numRanID).css('top')) - _list.eq(numRanID).height()
+            }, {
+                duration: duration,
+                complete: function () {
+                    _list.eq(numRanID).animate({
+                        top: -_list.eq(numRanID).height()
+                    }, {
+                        duration: duration,
+                        complete: function () {
+                            _list.eq(numRanID).css({
+                                top: _list.eq(numRanID).height()
+                            });
+                        }
+                    });
+                }
+            });
         }, speed);
 
         setTimeout(function () {
             clearInterval(timer);
+            _list.eq(numRan).animate({
+                top: 0
+            });
             isBegin = false;
         }, stop);
-
+        return numRan;
+    }
+    function initList() {
+        $('.list-group').each(function (i) {
+            if(i === 0){
+                $(this).css({
+                    top: 0
+                });
+            }else{
+                var top = parseInt($('.list-group:eq(' + (i) + ')').css('top')) + ($(this).height());
+                $(this).css({
+                    top: top
+                });
+            }
+        });
+    }
+    //抽取一次
+    $('.start').on('click', function(){
+        var winners = lottery();
+        setTimeout(function(){
+            $('#res').html('恭喜：' + $('.list-group').eq(winners).html());
+        },stop);
+        console.log(winners);
     });
 });
 
-function initList() {
-    $('.list-group').each(function (i) {
-        var top = parseInt($('.list-group:eq(' + (i) + ')').css('top')) + ($(this).height() * i);
-        $(this).css({
-            top: top
-        });
-    });
-}
-
-function easeInOutCirc(x, t, b, c, d) {
-    if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
-    return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
-}
